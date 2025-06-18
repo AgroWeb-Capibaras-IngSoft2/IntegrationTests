2ee939268a39a7f7d923c0c44f315320bc0c977b
SebasIron-1992 <joalarconb@unal.edu.co>
2025-06-17 19:58:50 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/loginservice'   // <-- Asegúrate de que la ruta coincida
+import Register from './components/Register'; // ruta correcta según tu estructura
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
       <Routes>
         {/* Ruta de login en “/” */}
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
         {/* Aquí luego agregarás más rutas, p.ej. Dashboard, Signup, etc. */}
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
